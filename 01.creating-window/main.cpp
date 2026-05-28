1e08a0802fa474687cce7072ffe48bc6ff05d823
Nihad <nihad.nemetli@gmail.com>
2026-05-28 22:41:26 +0400

    add: background color added

--- 01.creating-window/main.cpp
+++ 01.creating-window/main.cpp
@@ -40,6 +40,8 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         processInoput(window);
         glfwSwapBuffers(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwPollEvents();
     }
 
